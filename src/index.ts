392620e01c344c7a299ca2ef352049383cae63fd
Koppa Péter <koppa96@windowslive.com>
2023-03-28 23:08:55 +0200

    Implement groupBy

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,31 @@ import { where } from './operators/where'
 import { first } from './finishers/first'
 import { last } from './finishers/last'
 import { max } from './finishers/max'
+import { count } from './finishers/count'
+import { groupBy } from './operators/groupBy'
+
+interface Person {
+  name: string
+  age: number
+}
+
+const people: Person[] = [
+  { name: 'A', age: 16 },
+  { name: 'B', age: 23 },
+  { name: 'C', age: 16 },
+  { name: 'D', age: 11 },
+  { name: 'E', age: 23 },
+  { name: 'F', age: 16 },
+]
 
 const result = query(
-  from([1, 6, 3, 7, 5, 2]),
-  where(item => item > 3)
+  from(people),
+  groupBy(person => person.age)
 )
 
-const res2 = query(from(result), max())
-const fst = query(from(result), first())
-
-console.log({ res2, fst })
+for (const group of result) {
+  console.log(`Key: ${group.key}`)
+  for (const element of group) {
+    console.log(element.name)
+  }
+}
---
+++ src/operators/groupBy.ts
@@ -0,0 +1,36 @@
+import { query } from '../query'
+import { from } from '../starters/from'
+import { defaultEqualityCheck, EqualityCheck, Operator } from '../types'
+import { distinct } from './distinct'
+import { select } from './select'
+import { where } from './where'
+
+export type Grouping<K, T> = Iterable<T> & { key: K }
+
+export function groupBy<T, K>(
+  keySelector: (item: T) => K,
+  equalityCheck: EqualityCheck<K> = defaultEqualityCheck
+): Operator<T, Grouping<K, T>> {
+  return function (source) {
+    return {
+      *[Symbol.iterator]() {
+        const keys = query(
+          from(source),
+          select(keySelector),
+          distinct(equalityCheck)
+        )
+
+        for (const key of keys) {
+          yield {
+            key,
+            [Symbol.iterator]() {
+              return where<T>(element =>
+                equalityCheck(keySelector(element), key)
+              )(source)[Symbol.iterator]()
+            },
+          }
+        }
+      },
+    }
+  }
+}
