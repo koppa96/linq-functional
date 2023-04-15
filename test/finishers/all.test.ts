af3d800c1b85905742eeaadd22360e873893d643
Koppa Péter <koppa96@windowslive.com>
2023-04-15 11:09:55 +0200

    Add all tests

---
+++ test/finishers/all.test.ts
@@ -0,0 +1,31 @@
+import { assert, describe, it } from 'vitest'
+import { all } from '../../src'
+
+describe('all', () => {
+  it('is true when the iterable is empty', () => {
+    const operator = all<number>(value => value < 1)
+    const source: number[] = []
+
+    const result = operator(source)
+
+    assert.isTrue(result)
+  })
+
+  it('is false when an element does not match the predicate', () => {
+    const operator = all<number>(value => value < 1)
+    const source = [1, 2]
+
+    const result = operator(source)
+
+    assert.isFalse(result)
+  })
+
+  it('is true when all elements match the predicate', () => {
+    const operator = all<number>(value => value < 1)
+    const source = [0, 1]
+
+    const result = operator(source)
+
+    assert.isFalse(result)
+  })
+})
