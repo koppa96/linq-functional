b850c5568747222b13c166f0af8dd717f9199a6c
Koppa Péter <koppa96@windowslive.com>
2023-04-23 10:21:25 +0200

    Add documentation comments

--- src/lib/operators/append.ts
+++ src/lib/operators/append.ts
@@ -1,7 +1,35 @@
 import { Operator } from '../types'
 
+/**
+ * Creates a new `Iterable` that starts with the elements of the source
+ * iterable, and ends with the elements passed as argument.
+ * @param elements An iterable containing the elements to append
+ * @returns An operator configured by the given parameters
+ * @example
+ * const result = query(
+ *   from([1, 2, 3]),
+ *   append([4, 5, 6]),
+ *   toArray()
+ * )
+ * console.log(result) // Outputs [1, 2, 3, 4, 5, 6]
+ */
 export function append<T>(elements: Iterable<T>): Operator<T, T>
+
+/**
+ * Creates a new `Iterable` that starts with the elements of the source
+ * iterable, and ends with the elements passed as argument.
+ * @param elements The elements to append
+ * @returns An operator configured by the given parameters
+ * @example
+ * const result = query(
+ *   from([1, 2, 3]),
+ *   append(4, 5, 6),
+ *   toArray()
+ * )
+ * console.log(result) // Outputs [1, 2, 3, 4, 5, 6]
+ */
 export function append<T>(...elements: T[]): Operator<T, T>
+
 export function append<T>(elements: Iterable<T>): Operator<T, T> {
   return function (source) {
     return {
--- src/lib/operators/cast.ts
+++ src/lib/operators/cast.ts
@@ -1,5 +1,18 @@
 import { Operator } from '../types'
 
+/**
+ * Simply casts the elements of the source iterable to the desired type.
+ * Does no type checking. This operator is only supposed to be used if
+ * you know that the contents of the source sequence will only contain a
+ * specific type of elements. For other scenarios `ofType` is recommended.
+ * @returns An operator configured by the given parameters
+ * @example
+ * const numbers: unknown[] = [1, 2, 3]
+ * const result = query(
+ *   from(numbers),
+ *   cast<number>()
+ * ) // Has type Iterable<number>
+ */
 export function cast<T>(): Operator<unknown, T> {
   return function (source) {
     return source as Iterable<T>
