0c84f3983ad7a963be031c8d1befbf00aa4abdee
Koppa Péter <koppa96@windowslive.com>
2023-09-17 15:52:08 +0200

    Improve toArray efficiency

--- src/lib/finishers/toArray.ts
+++ src/lib/finishers/toArray.ts
@@ -13,10 +13,6 @@ import { Finisher } from '../types'
  */
 export function toArray<T>(): Finisher<T, T[]> {
   return function (source) {
-    const result: T[] = []
-    for (const element of source) {
-      result.push(element)
-    }
-    return result
+    return Array.from(source)
   }
 }
