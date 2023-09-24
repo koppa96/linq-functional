691caaf715b1e792f7b960d34e65b311e50d5d69
Koppa Péter <koppa96@windowslive.com>
2023-09-24 16:37:01 +0200

    Add more finisher docs

--- src/lib/finishers/count.ts
+++ src/lib/finishers/count.ts
@@ -1,18 +1,17 @@
 import { Finisher } from '../types'
 
 /**
- * Determines how many elements in the source sequence satisfy the given predicate.
- * If there is no predicate given, the operator will simply return the number of total elements in the sequence.
- * @param predicate A function that will test an element if it should be counted or not
+ * Creates a `Finisher` that determines how many items in the source `Iterable` satisfy the given predicate.
+ * If there is no predicate given, the operator will simply return the number of total items in the source.
+ * @param predicate A function that will test an item if it should be counted or not
  * @example
  * const result = query(
  *   from([1, 2, 3]),
  *   count(value => value < 3)
- * )
- * console.log(result) // Outputs 2
+ * ) // 2
  */
 export function count<T>(
-  predicate?: (element: T) => boolean
+  predicate?: (item: T) => boolean
 ): Finisher<T, number> {
   return function (source) {
     let result = 0
